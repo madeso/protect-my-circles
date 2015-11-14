09a79bb17f0f0489ebb3d32b1c98aa95c14d02fa
Gustav <sir.gustav.the.coder@gmail.com>
2015-11-14 11:25:09 +0100

    refactoring

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+	private bool can_move_ = false;
+
+	public float ThrowPower = 1;
+	
+	TouchManager touch_manager_;
+	bool is_picked_up_ = false;
+	int finger_id_ = TouchManager.NO_FINGER_ID;
+	
+	Vector3 throwing_ = new Vector3(0,0,0);
+	float throwing_power_ = -1.0f;
+
+	public bool CanMove {
+		get {
+			return this.can_move_;
+		}
+	}
+	
+	void Start () {	
+		this.touch_manager_ = GameObject.Find("TouchManager").GetComponent<TouchManager>();
+	}
+	
+	void Update () {
+		this.can_move_ = false;
+		if( this.throwing_power_ <= 0 ) {
+			if( this.is_picked_up_ == false ) {
+				this.can_move_ = true;
+			}
+		}
+		else {
+			this.transform.position += throwing_ * Time.deltaTime * this.throwing_power_;
+			this.throwing_power_ -= Time.deltaTime;
+			if( this.throwing_power_ <= 0.0f ) {
+				this.throwing_power_ = - 10;
+			}
+		}
+		
+		if( this.is_picked_up_ ) {
+			Vector3 throw_dir;
+			this.transform.position = this.touch_manager_.GetUpdatedPosition(ref this.finger_id_, ref this.is_picked_up_, out throw_dir);
+			
+			if( this.is_picked_up_ == false ) {
+				this.throwing_ = throw_dir;
+				this.throwing_power_ = this.ThrowPower;
+			}
+		}
+		else {
+			this.is_picked_up_ = this.touch_manager_.IsTouched(this.gameObject, out this.finger_id_);
+		}
+	}
+}
--- Assets/Scripts/RomanMovement.cs
+++ Assets/Scripts/RomanMovement.cs
@@ -6,46 +6,17 @@ public class RomanMovement : MonoBehaviour {
 
 	public float Speed = 1.0f;
 
-	public float THROW_POWER = 1;
-
-	TouchManager touch_manager_;
-	bool is_picked_up_ = false;
-	int finger_id_ = TouchManager.NO_FINGER_ID;
-	
-	Vector3 throwing_ = new Vector3(0,0,0);
-	float throwing_power_ = -1.0f;
+	Pickup pickup_;
 
 	void Start () {	
-		this.touch_manager_ = GameObject.Find("TouchManager").GetComponent<TouchManager>();
+		this.pickup_ = this.GetComponent<Pickup>();
 	}
 
 	void Update () {
-		if( this.throwing_power_ <= 0 ) {
-			if( this.is_picked_up_ == false ) {
-				var p = this.transform.position;
-				var walk_dir = (target_ - p).normalized;
-				this.transform.position += walk_dir * Time.deltaTime * Speed;
-			}
-		}
-		else {
-			this.transform.position += throwing_ * Time.deltaTime * this.throwing_power_;
-			this.throwing_power_ -= Time.deltaTime;
-			if( this.throwing_power_ <= 0.0f ) {
-				this.throwing_power_ = - 10;
-			}
-		}
-
-		if( this.is_picked_up_ ) {
-			Vector3 throw_dir;
-			this.transform.position = this.touch_manager_.GetUpdatedPosition(ref this.finger_id_, ref this.is_picked_up_, out throw_dir);
-
-			if( this.is_picked_up_ == false ) {
-				this.throwing_ = throw_dir;
-				this.throwing_power_ = this.THROW_POWER;
-			}
-		}
-		else {
-			this.is_picked_up_ = this.touch_manager_.IsTouched(this.gameObject, out this.finger_id_);
+		if( pickup_.CanMove ) {
+			var p = this.transform.position;
+			var walk_dir = (target_ - p).normalized;
+			this.transform.position += walk_dir * Time.deltaTime * Speed;
 		}
 	}
 }
